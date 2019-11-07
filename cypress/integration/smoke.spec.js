70bb6565bc7f9160e5cbdf0721cae424e2d920b4
olliebrownlow <olliebrownlow@gmail.com>
2019-11-07 16:26:15 +0000

    gets class properties working

--- cypress/integration/smoke.spec.js
+++ cypress/integration/smoke.spec.js
@@ -1,5 +1,5 @@
 describe("smoke test", () => {
   it("Makes sure the welcome message comes up", () => {
-    cy.visit("http://localhost:1234").contains("Hello, world!");
+    cy.visit("http://localhost:1234").contains("Hello, viewers!");
   });
 });
