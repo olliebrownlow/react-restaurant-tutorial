356da4b5f5c4e874af5028660d7c2f29d34b7c46
olliebrownlow <olliebrownlow@gmail.com>
2019-11-15 16:52:53 +0000

    adds in case when no restaurant has been added

--- src/RestaurantList.js
+++ src/RestaurantList.js
@@ -2,10 +2,16 @@ import React from "react";
 import { Collection, CollectionItem } from "react-materialize";
 
 const RestaurantList = ({ restaurantNames }) => (
-  <Collection>
-    {restaurantNames.map(restaurantName => (
-      <CollectionItem key={restaurantName}>{restaurantName}</CollectionItem>
-    ))}
+  <Collection header="Restaurants">
+    {restaurantNames.length === 0 ? (
+      <CollectionItem style={{ color: "gray" }}>
+        (none added yet)
+      </CollectionItem>
+    ) : (
+      restaurantNames.map(restaurantName => (
+        <CollectionItem key={restaurantName}>{restaurantName}</CollectionItem>
+      ))
+    )}
   </Collection>
 );
 
