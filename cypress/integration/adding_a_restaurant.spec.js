c858323e0e0165be0337800bff100496a1148739
olliebrownlow <olliebrownlow@gmail.com>
2019-12-15 09:25:34 +0000

    adds more description to e2e test

--- cypress/integration/adding_a_restaurant.spec.js
+++ cypress/integration/adding_a_restaurant.spec.js
@@ -4,25 +4,31 @@ describe("adding a restaurant", () => {
 
     cy.visit("http://localhost:1234");
 
-    // modal not shown on start up
+    // modal not shown on start
     cy.get('[data-test="newRestaurantName"]').should("not.be.visible");
 
+    // open modal
     cy.get('[data-test="addRestaurantButton"]').click();
 
     // modal can be cancelled
     cy.get('[data-test="addRestaurantModal"] button.modal-close').click();
 
+    // modal not shown again
     cy.get('[data-test="newRestaurantName"]').should("not.be.visible");
 
-    // modal allowsadding of a restaurant
+    // open modal
     cy.get('[data-test="addRestaurantButton"]').click();
 
+    // modal allows typing of a restaurant name
     cy.get('input[data-test="newRestaurantName"]').type(restaurantName);
 
+    // restaurant saved
     cy.get('[data-test="saveNewRestaurantButton"]').click();
 
+    // modal not shown again
     cy.get('[data-test="newRestaurantName"]').should("not.be.visible");
 
+    // added restaurant name visible
     cy.contains(restaurantName);
   });
 });
