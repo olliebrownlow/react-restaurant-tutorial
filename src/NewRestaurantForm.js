5a5540ca1468785685097972ea9d674e2495f40a
olliebrownlow <olliebrownlow@gmail.com>
2019-12-15 19:36:10 +0000

    clears out text field upon save

--- src/NewRestaurantForm.js
+++ src/NewRestaurantForm.js
@@ -13,6 +13,7 @@ export default class NewRestaurantForm extends Component {
     const { onSave } = this.props;
 
     onSave(inputText);
+    this.setState({ inputText: "" });
   };
 
   render() {
